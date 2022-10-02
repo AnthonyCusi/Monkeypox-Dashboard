fbf04790702e8bacbc0c6ff66c0b37f9a2998e9d
Anthony Cusimano <anthonyc.cusimano@gmail.com>
2022-10-01 18:00:05 -0700

    Fixed issue due to deprecated data

--- data_loader.py
+++ data_loader.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 def load_full_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest_deprecated.csv')
 
     df.drop(['Source', 'Source_II', 'Source_III', 'Source_IV', 'Source_V', 'Source_VI', 'Source_VII',
         'ID', 'Contact_ID', 'Contact_comment', 'Date_last_modified'], axis = 1, inplace = True)
@@ -12,14 +12,14 @@ def load_full_data():
     return df
 
 def load_cumulative_cases():
-    df = pd.read_csv('https://raw.githubusercontent.com/globaldothealth/monkeypox/main/timeseries-country-confirmed.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/globaldothealth/monkeypox/main/timeseries-country-confirmed-deprecated.csv')
     
     # Formatting information for readability
     df.rename(columns = {'Cumulative_cases': 'Cumulative Cases'}, inplace = True)
     return df
 
 def load_total_cases():
-    df = pd.read_csv('https://raw.githubusercontent.com/globaldothealth/monkeypox/main/timeseries-confirmed.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/globaldothealth/monkeypox/main/timeseries-confirmed-deprecated.csv')
 
     # Formatting information for readability
     df.rename(columns = {'Cumulative_cases': 'Cumulative Cases'}, inplace = True)
